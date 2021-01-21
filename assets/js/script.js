b8819375c3c13b5a73cd2a784c2dc78e39976490
pythagoras-dev <mubarakmustapha15@gmail.com>
2021-01-21 16:56:56 +0100

    added email verification

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,3 +101,27 @@ function toggleAccordion() {
 }
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+
+// email
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function validate() {
+    const $result = $("#result");
+    const email = $("#email").val();
+    $result.text("");
+
+    if (validateEmail(email)) {
+        $result.text("Thanks! for contacting us");
+        $result.css("color", "green");
+    } else {
+        $result.text("Whoops,make sure it's an email");
+        $result.css("color", "red");
+    }
+    return false;
+}
+
+$("#validate").on("click", validate);
